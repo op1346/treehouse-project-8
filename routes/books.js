d73aae86c0d53ca6b53a837edbf62239ea09be58
Olivia Park <olivia.park1346@gmail.com>
2020-08-12 16:50:24 +0900

    adding to books.js

--- routes/books.js
+++ routes/books.js
@@ -1,24 +1,41 @@
-const Sequelize = require('sequelize');
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
 
-module.exports = (sequelize) => {
-  class Books extends Sequelize.Model {}
-  Book.init({
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: Sequelize.STRING,
-    },
-    author: {
-      type: Sequelize.STRING,
-    },
-    genre: {
-      type: Sequelize.STRING,
-    },
-    year: {
-      type: Sequelize.INTEGER,
-    },
-{sequelize});
-  return Movie;
+//Handler function
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error) {
+      res.status(500).send(error);
+    }
+  }
+}
+
+//Home route redirect to books route
+
+//GET book listing
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.render("/index", {books, title: "SQL Library Manager"});
+}));
+
+//Create a new Book form
+router.get('/new', (req, res) => {
+  res.render("/new-book", { book: {}, title: "New Book"});
+});
+
+//POST create Book
+router.post('/', asyncHandler(async (req, res)=> {
+  const book = await Book.create();
+  res.redirect("/")
+}))
+
+//GET individual Book detail
+
+//Updates book
+
+//Delete book
+
+module.exports = router;
